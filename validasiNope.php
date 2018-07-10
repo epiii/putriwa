862bb19003a5cb98d84e8dab5d6438b9359fdd51
epiii <gue.elfrianto@gmail.com>
2018-07-11 04:16:33 +0700

    ok

--- validasiNope.php
+++ validasiNope.php
@@ -7,17 +7,12 @@ if(!isset($_POST['no'])){ // error
   $out=['status'=>'invalid_request'];
 } else { // tidak error
   $readNo = read_no($_POST['no']);
-  // $out=$readNo;
-  // if(is_null($readNo['warning'])){
-    $out=[
-      'status'=>'valid_request',
-      'warning'=>$readNo['warning'],
-      'nomor'=>$readNo['nomor'],
-      'negara'=>$readNo['negara'],
-    ];
-  // }else{
-  //   $out=['warning'=>$readNo['warning']];
-  // }
+  $out=[
+    'status'=>'valid_request',
+    'warning'=>$readNo['warning'],
+    'nomor'=>$readNo['nomor'],
+    'negara'=>$readNo['negara'],
+  ];
 }
 echo json_encode($out);
 ?>
